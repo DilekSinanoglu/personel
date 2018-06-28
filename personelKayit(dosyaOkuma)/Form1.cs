fed4de6df884588d2dc52788cf366488b4128db5
Dilek Sinanoglu <dileksinanoglu97@gmail.com>
2018-06-28 16:45:08 +0300

    deneme

--- personelKayit(dosyaOkuma)/Form1.cs
+++ personelKayit(dosyaOkuma)/Form1.cs
@@ -1,15 +1,6 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Serialization;
 
 namespace personelKayit_dosyaOkuma_
 {
